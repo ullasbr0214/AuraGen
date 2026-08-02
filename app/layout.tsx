344c782f6aa3543756c43297171ebab95c25c44d
ullasbr014 <ullasbr0292004@gmail.com>
2026-08-02 12:15:55 +0530

    Update AuraGen

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import "./globals.css";
 
-import { AuthProvider } from "./context/AuthContext";
-import { TelemetryProvider } from "./context/TelemetryContext";
 import { AuraProvider } from "./context/AuraContext";
 import { ChatProvider } from "./context/ChatContext";
+import { TelemetryProvider } from "./context/TelemetryContext";
+
 import { Toaster } from "react-hot-toast";
 
 export default function RootLayout({
@@ -14,28 +14,24 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-  <AuthProvider>
-    <TelemetryProvider>
-      <AuraProvider>
 
-        {children}
+        <AuraProvider>
 
-        <Toaster
-          position="top-right"
-          toastOptions={{
-            duration: 3000,
-            style: {
-              background: "#0f172a",
-              color: "#fff",
-              border: "1px solid #06b6d4",
-            },
-          }}
-        />
+          <ChatProvider>
 
-      </AuraProvider>
-    </TelemetryProvider>
-  </AuthProvider>
-</body>
+            <TelemetryProvider>
+
+              {children}
+
+              <Toaster position="top-right" />
+
+            </TelemetryProvider>
+
+          </ChatProvider>
+
+        </AuraProvider>
+
+      </body>
     </html>
   );
 }
--- app/services/api.ts
+++ app/services/api.ts
@@ -1,7 +1,11 @@
 import axios from "axios";
 
+const API_URL =
+  process.env.NEXT_PUBLIC_API_URL ||
+  "https://cover-patriot-overhand.ngrok-free.dev/api";
+
 const api = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: API_URL,
   headers: {
     "Content-Type": "application/json",
   },
